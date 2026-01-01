0a1cdd4397e77fa510692bcde85b0779f91f3ea8
Hennadii_Shpak <hennadii_shpak@epam.com>
2026-01-01 15:37:53 +0200

    finish homework 9.3. space remover

--- Homework_9.1/Homework_9.1/Program.cs
+++ Homework_9.1/Homework_9.1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Homework_9;
 using Homework_9._1.Solutions;
 
-var isTheSameStart = Homework_9_1.FullNameComparison("Hennadii Shpak");
+//var isTheSameStart = Homework_9_1.FullNameComparison("Hennadii Shpak");
 
-TextReport.GenerateTextReport();
+//TextReport.GenerateTextReport();
+
+SpaceRemover.RemoveSpace("Happy, new, year, !!!! ");
---
+++ Homework_9.1/Homework_9.1/Solutions/SpaceRemover.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Homework_9._1.Solutions
+{
+    class SpaceRemover
+    {
+        public static void RemoveSpace(string userInput)
+        {
+            var normalizedInput = new StringBuilder(userInput).Replace(" ", "").ToString();
+
+            Console.WriteLine(normalizedInput);
+        }
+    }
+}
