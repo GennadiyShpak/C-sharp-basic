b906f5e5e38aefe3e80180e605d432673847e135
Hennadii_Shpak <hennadii_shpak@epam.com>
2026-01-05 21:27:11 +0200

    finish homework-11

---
+++ Homework-11/Homework-11/Program.cs
@@ -0,0 +1,30 @@
+﻿Calculator.CalculateFibonacci();
+
+class Calculator
+{
+
+
+    private static int GetFibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+    }
+
+    public static void CalculateFibonacci()
+    {
+        Console.WriteLine("Provide nubmer");
+        var userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int borderNumber))
+        {
+            var result = GetFibonacci(borderNumber);
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine("Your number incorrect");
+        }
+    }
+}
