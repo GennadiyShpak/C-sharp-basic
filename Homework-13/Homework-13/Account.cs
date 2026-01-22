ff8ae6b3e7aa2d2dc4ccbe66d7ef1828d9ccbde9
Hennadii_Shpak <hennadii_shpak@epam.com>
2026-01-22 10:56:30 +0200

    finish homework-13

---
+++ Homework-13/Homework-13/Account.cs
@@ -0,0 +1,57 @@
+﻿namespace Homework_13;
+
+internal class Account
+{
+    public decimal Amount { get; private set; } = 0;
+    public string Name { get; private set; } = string.Empty;
+
+    public Account (string name, decimal amount)
+    {
+        Amount = amount;
+        Name = name;
+    }
+
+    public void Withdrawal(decimal amount) {
+        if (!IsWithdrawalPossible(amount)) {
+            ShowError();
+        } else
+        {
+            Amount -= amount;
+            ShowSuccess();
+        }
+        
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (!IsDepositPossible(amount))
+        {
+            ShowError();
+        } else
+        {
+            Amount += amount;
+            ShowSuccess();
+        }
+        
+    }
+
+    private bool IsWithdrawalPossible(decimal amount)
+    {
+        return Amount >= amount && amount >= 0;
+    }
+
+    private bool IsDepositPossible(decimal amount)
+    {
+        return amount > 0;
+    }
+
+    private void ShowSuccess()
+    {
+        Console.WriteLine("Your operational was success!");
+    }
+
+    private void ShowError()
+    {
+        Console.WriteLine("You provided incorrect ammountm please try again");
+    }
+}
---
+++ Homework-13/Homework-13/Program.cs
@@ -0,0 +1,10 @@
+﻿using Homework_13;
+
+Account heikkisAccount = new Account("Heikki's account", 100.00m);
+Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00m);
+
+
+heikkisAccount.Withdrawal(20);
+Console.WriteLine($"The balance of {heikkisAccount.Name} account is now: {heikkisAccount.Amount}");
+heikkisSwissAccount.Deposit(200);
+Console.WriteLine($"The balance of {heikkisSwissAccount.Name} account is now: {heikkisSwissAccount.Amount}");
